0a3ff19b04f5ca60044bf910d5aa04e093a6a3ae
cskram1 <cskram1@ilstu.edu>
2018-03-08 16:16:59 -0600

    Add files via upload

--- DBConnect.java
+++ DBConnect.java
@@ -10,15 +10,28 @@ public class DBConnect {
 			
 			String hostname = "oracle-instance1.cprjhicmmemt.us-east-2.rds.amazonaws.com";
 			String port = "1521";
-			String dbName = "oracle-instance1";
+			String dbName = "ORCL";
 			String userName = "admin";
 			String password = "password";
-			String jdbcUrl = "jdbc:oracle:thin//" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
+			//String jdbcUrl = "jdbc:oracle:thin:@" + hostname + ":" + port + ":" + dbName + "?user=" + userName + "&password=" + password;
+			String jdbcUrl = "jdbc:oracle:thin:@oracle-instance1.cprjhicmmemt.us-east-2.rds.amazonaws.com:1521:ORCL";
+			Connection con = DriverManager.getConnection(jdbcUrl,userName,password);
+			System.out.print("it worked!");
+			String sql1 = "SELECT * FROM CUSTOMER";
+			PreparedStatement pr = con.prepareStatement(sql1);
+			ResultSet set = pr.executeQuery();
+			while (set.next())
+			{
+				String name = set.getString(1);
+				String name1 = set.getString(2);
+				System.out.print(name);
+				System.out.print(name1);
+			}
 			
-			Connection con = DriverManager.getConnection(jdbcUrl);
+			con.close();
+			pr.close();
 		}
 		catch ( SQLException err ) {System.out.println( err.getMessage( ) );} 
-		
 		
 	}
 
